3cc801055760b09085e7f4bdecb60a780c484d55
TheSteveIAm <TheSteveIAm@Gmail.com>
2020-02-12 19:36:38 -0600

    Finished vector3 class

--- Ray Tracing/Source.cpp
+++ Ray Tracing/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "vec3.h"
 using namespace std;
 
 int main()
@@ -7,19 +8,16 @@ int main()
 	int nx = 200;
 	int ny = 100;
 
-	
 	ofstream ppmFile;
 	ppmFile.open ("helloworld.ppm");
 
 	ppmFile << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 			ppmFile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
