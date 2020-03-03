491bde3d7ad0936e2546f20691e65d2e0939315b
TheSteveIAm <TheSteveIAm@Gmail.com>
2020-03-02 22:14:09 -0600

    Diffuse material

--- Ray Tracing/Source.cpp
+++ Ray Tracing/Source.cpp
@@ -6,10 +6,23 @@
 #include "camera.h"
 using namespace std;
 
+float random_zero_to_one() {
+	return ((double)rand() / (RAND_MAX));
+}
+
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0 * vec3(random_zero_to_one(), random_zero_to_one(), random_zero_to_one()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
+
 vec3 color(const ray& r, hitable* world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	if (world->hit(r, 0.001, FLT_MAX, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color( ray(rec.p, target-rec.p), world);
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -39,8 +52,8 @@ int main()
 			vec3 col(0, 0, 0);
 			for (int s = 0; s < ns; s++) {
 
-				float u = float(i + rand() / 25000) / float(nx);
-				float v = float(j + rand() / 25000) / float(ny);
+				float u = float(i + random_zero_to_one()) / float(nx);
+				float v = float(j + random_zero_to_one()) / float(ny);
 
 				ray r = cam.get_ray(u, v);
 				vec3 p = r.point_at_parameter(2.0);
@@ -48,6 +61,7 @@ int main()
 			}
 
 			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
